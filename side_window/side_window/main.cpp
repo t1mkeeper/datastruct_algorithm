7569fd6846c81f4f6aca944bf0a46a8d1c7691dc
t1mkeeper <t1mkeeper4869@gmail.com>
2021-02-06 13:21:15 +0800

    滑动窗口

---
+++ side_window/side_window/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  side_window
+//
+//  Created by t1mkeeper on 2021/2/4.
+//
+
+#include <iostream>
+#include "min_window_substring.hpp"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    std::string res = minWindow(std::string("abbencafdca"), std::string("aac"));
+    
+    return 0;
+}
---
+++ side_window/side_window/min_window_substring.hpp
@@ -0,0 +1,16 @@
+//
+//  min_window_substring.hpp
+//  side_window
+//
+//  Created by t1mkeeper on 2021/2/4.
+//
+
+#ifndef min_window_substring_hpp
+#define min_window_substring_hpp
+
+#include <stdio.h>
+#include <string>
+
+std::string minWindow(std::string s, std::string t);
+
+#endif /* min_window_substring_hpp */
