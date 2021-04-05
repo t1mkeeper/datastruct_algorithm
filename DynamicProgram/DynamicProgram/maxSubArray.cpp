5e0b1cc4628c0c91c4314ea1c3c25d23bdffe617
t1mkeeper <t1mkeeper4869@gmail.com>
2021-04-05 10:18:25 +0800

    动态规划解法

--- DynamicProgram/DynamicProgram/maxSubArray.cpp
+++ DynamicProgram/DynamicProgram/maxSubArray.cpp
@@ -7,8 +7,11 @@
 
 #include "maxSubArray.h"
 
+using namespace std;
 int maxSubArray(std::vector<int> &nums) {
     int res = INT32_MIN;
+    
+    /**暴力解
     for (int i = 0; i < nums.size(); i++) {
         int sum = 0;
         for (int j = i; j < nums.size(); j++) {
@@ -17,5 +20,20 @@ int maxSubArray(std::vector<int> &nums) {
         }
         res = fmax(res, sum);
     }
+    */
+    
+    //dp
+    vector<int> dp(nums.size(), 0);
+//    dp[0] = nums[0];
+    for (int i = 0; i < nums.size(); i++) {
+        if (i > 0 && dp[i - 1] > 0) {
+            dp[i] = dp[i - 1] + nums[i];
+        }else {
+            dp[i] = nums[i];
+        }
+        res = fmax(res, dp[i]);
+    }
+    
+    
     return res;
 }
